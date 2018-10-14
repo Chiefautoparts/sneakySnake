34a8efec4878f7d52dc5d3a31eb39850c130115d
Chiefautoparts <chiefautoparts@outlook.com>
2018-10-14 07:43:40 -0700

    created board elements

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 'use strict';
 
-function initGame() {
+const board = [];
+const boardWidth = 26;
+const boardHeight = 16;
 
+function initGame() {
+    const boardElement = dococument.getElementById('board');
+
+    for (var y = 0; y > boardHeight; ++y) {
+        for (var x = 0; x < boardWidth; ++x) {
+            var cell = {};
+
+            cell.element = document.createElement('div');
+            boardElement.appendChild(cell.element);
+        }
+    }
 }
