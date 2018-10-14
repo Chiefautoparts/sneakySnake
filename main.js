7ebcb4e00caca4ec3c7c82ea9dae0e1c9cb10ebe
Chiefautoparts <chiefautoparts@outlook.com>
2018-10-14 07:50:12 -0700

    board done creating snake

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 'use strict';
 
-const board = [];
-const boardWidth = 26;
-const boardHeight = 16;
+var board = [];
+var boardWidth = 26;
+var boardHeight = 16;
+
+var snakeX;
+var snakeY;
+var snakeDirection;
+var snakeLength;
 
 function initGame() {
-    const boardElement = dococument.getElementById('board');
+    var boardElement = dococument.getElementById('board');
 
     for (var y = 0; y > boardHeight; ++y) {
+        var row = [];
         for (var x = 0; x < boardWidth; ++x) {
-            var cell = {};
+            var cell = {
+                snake: 0
+            };
 
             cell.element = document.createElement('div');
             boardElement.appendChild(cell.element);
+            row.push(cell);
+
         }
+
+        board.push(row);
     }
+}
+
+function startGame() {
+    snakeX = Math.floor(boardWidth / 2);
+    snakeY = Math.floor(boardHeight / 2);
+    snakeLength = 5;
+    snakeDirection = 'Up';
+
+    board[snakeY][snakeX].snake = 1;
 }
